c397cebd442880937c906ef25f6443803abc1e17
ronalcabrera <ronal.cabrera@gmail.com>
2023-02-16 00:07:38 -0300

    actualizacion API

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM tiangolo/uvicorn-gunicorn-fastapi
 
-RUN pip install pandas
-RUN pip install numpy
-
 EXPOSE 80
 
 COPY ./app /app
---
+++ app/modelos.py
@@ -0,0 +1,5 @@
+import json
+
+denso = json.load(open("./exportacion/modelo_denso/model.json"))
+CNN = json.load(open("./exportacion/modelo_CNN/model.json"))
+CNN_AD_DO = json.load(open("./exportacion/modelo_CNN_AD_DO/model.json"))
